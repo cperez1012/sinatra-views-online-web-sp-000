15d584949bb0a7879f3e62919ac4426846002d25
Christian Perez <perezchristian1012@gmail.com>
2019-12-02 21:31:33 +0000

    Automatically backed up by Learn

---
+++ app.rb
@@ -0,0 +1,16 @@
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+	get '/' do
+		"<h1>Hello World</h1>"
+	end
+
+	get '/' do
+		erb :index
+	end
+
+	get '/info' do
+		"Testing the info page"
+	end
+end
